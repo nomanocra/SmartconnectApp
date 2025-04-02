6c0111a5df27b1e6127920234b677c241ffe96df
Noman <dprt.david@gmail.com>
2025-04-02 16:22:29 +0200

    feat(i18n): implement multilanguage support with vue-i18n

---
+++ src/lang/i18n-settings.js
@@ -0,0 +1,22 @@
+export default {
+  fr: {
+    login: {
+      user: 'Utilisateur',
+      password: 'Mot de passe',
+      title: 'SmartConnect App',
+      emailAdress: 'Adresse Email',
+      loginSubtitle: 'Connectez-vous à votre compte',
+      loginButton: 'Se connecter',
+    },
+  },
+  en: {
+    login: {
+      user: 'User',
+      password: 'Password',
+      emailAdress: 'Email address',
+      title: 'SmartConnect App',
+      loginSubtitle: 'Log in to your account',
+      loginButton: 'Login',
+    },
+  },
+}
--- src/main.js
+++ src/main.js
@@ -9,6 +9,8 @@ import 'primeicons/primeicons.css'
 import { definePreset } from '@primeuix/themes'
 import Aura from '@primeuix/themes/aura'
 import { SmartTheme } from './assets/styles/tokens/SmartTheme'
+import { createI18n } from 'vue-i18n'
+import i18nSettings from './lang/i18n-settings'
 const app = createApp(App)
 
 const MyPreset = definePreset(Aura, SmartTheme)
@@ -22,6 +24,26 @@ app.use(PrimeVue, {
   },
 })
 
+// Récupérer la langue du système
+const getSystemLanguage = () => {
+  // On vérifie d'abord si une langue est stockée dans le localStorage
+  const storedLanguage = localStorage.getItem('language')
+  if (storedLanguage && ['fr', 'en'].includes(storedLanguage)) {
+    return storedLanguage
+  }
+  const browserLang = navigator.language.split('-')[0] // 'fr-FR' devient 'fr'
+  // On vérifie si la langue du système est supportée, sinon on utilise 'en' par défaut
+  return ['fr', 'en'].includes(browserLang) ? browserLang : 'en'
+}
+
+const i18n = createI18n({
+  locale: getSystemLanguage(),
+  fallbackLocale: 'en',
+  messages: i18nSettings,
+})
+
 // app.use(PrimeVue, { unstyled: true })
 app.use(router)
+app.use(i18n)
+
 app.mount('#app')
