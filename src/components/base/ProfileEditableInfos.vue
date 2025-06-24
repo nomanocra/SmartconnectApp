a93ee81710e105a00cad69b97fa4305960594fec
Noman <dprt.david@gmail.com>
2025-06-24 11:14:51 +0200

    feat(profile): add user profile components and editable info

---
+++ src/components/base/ProfileEditableInfos.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="editable_info">
+    <label for="label">{{ props.label }}</label>
+    <span class="non-editable-value" v-if="!props.editable" id="value">{{ props.value }}</span>
+    <div v-else class="editable-value" @click="handleEdit">
+      <span id="value">{{ props.value }}</span>
+      <i class="pi pi-pencil" style="font-size: 0.75rem"></i>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+  value: {
+    type: String,
+    required: true,
+  },
+  editable: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+const emit = defineEmits(['handleEdit'])
+const handleEdit = () => {
+  emit('handleEdit')
+}
+</script>
+
+<style scoped>
+.editable_info {
+  display: flex;
+  flex-direction: column;
+  gap: 0.125rem;
+  max-width: fit-content;
+}
+
+label {
+  font-size: 1rem;
+  font-weight: 500;
+  color: var(--p-text-primary-color);
+}
+
+span {
+  font-size: 0.875rem;
+  font-weight: 400;
+  color: var(--p-text-secondary-color);
+}
+
+.non-editable-value {
+  padding: var(--size-content-padding-xs);
+  cursor: default;
+}
+
+.editable-value {
+  display: flex;
+  align-items: center;
+  gap: var(--size-content-gap-xs);
+  cursor: pointer;
+  padding: var(--size-content-padding-xs);
+  border-radius: var(--p-button-border-radius);
+  .pi-pencil {
+    color: var(--p-text-secondary-color);
+    opacity: 0.3;
+  }
+
+  &:hover {
+    background-color: var(--p-button-text-primary-hover-background);
+    .pi-pencil {
+      opacity: 1;
+    }
+  }
+  &:active {
+    background-color: var(--p-button-text-primary-active-background);
+  }
+}
+</style>
---
+++ src/components/features/UserProfile.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="user-profile">
+    <Avatar label="D" class="user-avatar" />
+    <ProfileEditableInfos label="Full Name" :value="userInfos.fullName" editable />
+    <ProfileEditableInfos label="Email" :value="userInfos.email" />
+    <ProfileEditableInfos label="Role" :value="userInfos.role" />
+    <ProfileEditableInfos label="Organization" :value="userInfos.organisationName" />
+    <Button
+      icon="pi pi-sign-out"
+      iconPos="right"
+      label="Sign out"
+      class="button-primary"
+      variant="text"
+    ></Button>
+  </div>
+</template>
+
+<script setup>
+import { inject } from 'vue'
+import Avatar from 'primevue/avatar'
+import ProfileEditableInfos from '@/components/base/ProfileEditableInfos.vue'
+import Button from 'primevue/button'
+const userInfos = inject('userInfos')
+</script>
+
+<style scoped>
+.user-profile {
+  display: flex;
+  flex-direction: column;
+  gap: 1.375rem;
+}
+
+.user-avatar {
+  width: 8rem;
+  height: 8rem;
+  font-size: 3.5rem;
+}
+</style>
