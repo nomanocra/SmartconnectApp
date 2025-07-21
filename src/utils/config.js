113e43ea875488edbe36c2588720854261b56c1f
Noman <dprt.david@gmail.com>
2025-07-21 18:35:48 +0200

    fix[demoversion]: Fix bug not taking in account demoversion variable env.

--- src/utils/config.js
+++ src/utils/config.js
@@ -6,7 +6,7 @@ export const config = {
     import.meta.env.MODE === 'development'
       ? import.meta.env.VITE_API_BASE_URL_DEV
       : import.meta.env.VITE_API_BASE_URL_PROD || import.meta.env.VITE_API_BASE_URL,
-  demoVersion: import.meta.env.VITE_DEMO_VERSION,
+  demoVersion: import.meta.env.VITE_DEMO_VERSION === 'true',
 }
 
 export default config
